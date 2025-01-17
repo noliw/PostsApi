daa211c1a28c59ff0568a5f2de68ab4f89b6d520
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-01-17 03:05:18 -0800

    creting the posts api service

--- app/src/main/java/com/nolawiworkineh/countriesapi/data/network/PostsApiService.kt
+++ app/src/main/java/com/nolawiworkineh/countriesapi/data/network/PostsApiService.kt
@@ -1,2 +1,9 @@
 package com.nolawiworkineh.countriesapi.data.network
 
+import com.nolawiworkineh.countriesapi.data.PostsModel
+import retrofit2.http.GET
+
+interface PostsApiService {
+    @GET("posts")
+    suspend fun getPosts(): List<PostsModel>
+}
