1c456a6043b27ad3fb122b891e1c2be3e9234215
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-01-17 03:41:28 -0800

    renamed network response

--- app/src/main/java/com/nolawiworkineh/countriesapi/data/network/util/NetworkResult.kt
+++
@@ -1,7 +0,0 @@
-package com.nolawiworkineh.countriesapi.data.network.util
-
-sealed class NetworkResult<out T> {
-    data class Success<out T>(val data: T) : NetworkResult<T>()
-    data class Error(val message: String) : NetworkResult<Nothing>()
-    object Loading : NetworkResult<Nothing>()
-}
