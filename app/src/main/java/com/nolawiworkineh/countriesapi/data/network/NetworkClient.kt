558b2e912a1e679c5e994b1cd30e75367247d47f
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-01-17 02:47:11 -0800

    Creating the network Client

--- app/src/main/java/com/nolawiworkineh/countriesapi/data/network/NetworkClient.kt
+++ app/src/main/java/com/nolawiworkineh/countriesapi/data/network/NetworkClient.kt
@@ -6,9 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 object NetworkClient {
     private const val BASE_URL =  "https://jsonplaceholder.typicode.com/"
 
-    fun createRetrofit(): Retrofit =
+    val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
 }
